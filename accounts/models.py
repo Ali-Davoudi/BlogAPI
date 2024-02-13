e584267968bc6e1e49a33ab6ff2a35c2c8807c81
Ali Davoudi <Ali.Davoudi.A@gmail.com>
2024-02-14 01:22:46 +0330

    Modify model name:User to CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     name = models.CharField(max_length=200, null=True, blank=True)
